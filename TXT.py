19182b9f11887a779039267aabc9953e1f532986
joohojin <91173865+joohojin@users.noreply.github.com>
2022-11-23 20:56:28 +0900

    Update TXT.py
    경로를 재탐색 합니다.

--- TXT.py
+++ TXT.py
@@ -2,11 +2,11 @@ import os
 import glob
 
 # 파일들이 있는 경로
-path = r'F:\푸앙봇\020.주제별 텍스트 일상 대화 데이터\01.데이터\2.Validation\원천데이터\VS_01. KAKAO'
+path = r'여기에 경로 입력'
 
 # 파일들을 읽어서 하나의 파일로 만들기
 files = glob.glob(os.path.join(path, '*.txt'))
-with open(os.path.join(path, 'KAKAO-Data.txt'), 'w', encoding='utf-8') as outfile:
+with open(os.path.join(path, '여기에 텍스트 파일 이름 입력.txt'), 'w', encoding='utf-8') as outfile:
     for fname in files:
         with open(fname, encoding='utf-8') as infile:
             outfile.write(infile.read() + '\n')
