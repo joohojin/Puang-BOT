d81166edea52f00001576dee876c504c225b29cf
joohojin <joohojin2003@naver.com>
2022-12-13 00:36:42 +0900

    으아아ㅏㅏㅏ
    으아아아아아아ㅏㅏㅏㅏ

---
+++ import main as m.py
@@ -0,0 +1,18 @@
+# 디스코드 봇을 실행시키는 puangbot.py와 챗봇을 담당하는 chat_bot.py를 불러옵니다.
+
+
+# 디스코드 봇을 실행시키는 puangbot.py
+import puangbot as pu
+
+# 챗봇을 담당하는 chat_bot.py
+import chat_bot as cb
+
+# 디스코드 봇을 실행시킵니다.
+pu.run()
+
+# 챗봇을 실행시킵니다.
+
+cb.run()
+
+
+
