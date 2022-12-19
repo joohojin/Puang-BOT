0e16927c2d562c454c571ded5eb5881230be9989
sungjae127 <117696109+sungjae127@users.noreply.github.com>
2022-12-19 18:17:16 +0900

    description_test2

--- Puang_no2.py
+++ Puang_no2.py
@@ -11,7 +11,7 @@ async def on_ready():
     print("{0.user} online.")
     
 @bot.slash_command(guild_ids = [1039072581237624952], name = "푸앙 제 2호", description = "푸앙 제 2호와 인사하기")
-async def teach2(ctx, 메시지:Option("할 말 입력")):
+async def teach2(ctx, 메시지:Option(str, "할 말 입력")):
     await ctx.respond(ctx.author.name +  "님 :" + 메시지)
     
 bot.run('토큰')
