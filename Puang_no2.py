92ff9f79ab76e4045352327d8a973c2d4cd2e72f
sungjae127 <117696109+sungjae127@users.noreply.github.com>
2022-12-19 18:53:03 +0900

    description_test3

--- Puang_no2.py
+++ Puang_no2.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from discord.commands import Option
 
 intents = discord.Intents.all()
 
@@ -11,7 +10,7 @@ async def on_ready():
     print("{0.user} online.")
     
 @bot.slash_command(guild_ids = [1039072581237624952], name = "푸앙 제 2호", description = "푸앙 제 2호와 인사하기")
-async def teach2(ctx, 메시지:Option(str, "할 말 입력")):
+async def teach2(ctx, 메시지:str(description = "할 말 입력")):
     await ctx.respond(ctx.author.name +  "님 :" + 메시지)
     
 bot.run('토큰')
